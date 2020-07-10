2491337a7391daf2d863e3225995c9051cf612ad
Sebastian Bergmann <sb@sebastian-bergmann.de>
2020-07-10 11:17:50 +0200

    Initial work on Complexity value object

---
+++ tests/unit/ComplexityTest.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of sebastian/complexity.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace SebastianBergmann\Complexity;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \SebastianBergmann\Complexity\Complexity
+ */
+final class ComplexityTest extends TestCase
+{
+    public function testHasName(): void
+    {
+        $this->assertSame('Foo::bar', $this->complexity()->name());
+    }
+
+    public function testHasCyclomaticComplexity(): void
+    {
+        $this->assertSame(1, $this->complexity()->cyclomaticComplexity());
+    }
+
+    private function complexity(): Complexity
+    {
+        return new Complexity('Foo::bar', 1);
+    }
+}
